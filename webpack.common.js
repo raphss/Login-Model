053fa46b94be153d99db4c3f0ca6fbb6d5aaf944
raphss <raphvilete@gmail.com>
2024-03-19 03:21:02 -0300

    babel and postcss added

--- webpack.common.js
+++ webpack.common.js
@@ -2,45 +2,54 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-
-    module: {
-        rules: [{
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource',
-            },
-        ],
-    },
-
-    entry: {
-        index: './src/index.js',
-    },
-
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/template.html',
-            title: 'Login Model',
-            filename: 'index.html',
-            inject: 'body',
-            scriptLoading: 'defer',
-        }),
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', { targets: '> 1%, not dead' }]],
+          },
+        },
+      },
     ],
+  },
 
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
+  entry: {
+    babel: './src/babel.js',
+  },
 
-    optimization: {
-        runtimeChunk: 'single',
-    },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+      title: 'Login Model',
+      filename: 'index.html',
+      inject: 'body',
+      scriptLoading: 'defer',
+    }),
+  ],
 
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+
+  optimization: {
+    runtimeChunk: 'single',
+  },
 };
